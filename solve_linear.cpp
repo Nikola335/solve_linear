1578af4ef60cd65bab4c3e4e96cb8d9f9b72d375
root <root@v1644201.hosted-by-vdsina.ru>
2023-02-07 22:51:33 +0300

    first commit

---
+++ solve_linear.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+// Решаем уравнение ax + b = 0.
+double solve_linear(double a, double b)
+{
+    // Неужели так просто?
+    return -b / a;
+}
+
+int main()
+{
+    cout << "Solving ax + b = 0, enter a, b:\n";
+    cout.precision(16);
+    for (double a, b; cin >> a >> b;)
+        cout << "x == " << solve_linear(a, b) << endl;
+
+    return EXIT_SUCCESS;
+}
